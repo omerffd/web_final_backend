30faf9592185a92f0343540ab89f6379373e1c42
omerffd <omerfdogan.tr@gmail.com>
2025-01-14 12:55:33 +0300

    File Backend Project

---
+++ dist/src/api/listing/services/listing.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * listing service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::listing.listing');
